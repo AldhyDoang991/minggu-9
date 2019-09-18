7da84cd1fc4271e6a1fcb21ece9864d6196d0e06
aldhy <53283631+AldhyDoang991@users.noreply.github.com>
2019-09-18 13:44:32 +0700

    Create RemoveAllValueArray.js

---
+++ RemoveAllValueArray.js
@@ -0,0 +1,13 @@
+let bulan = ['januari', 'february', 'maret', 'april', 'juni', 'juli', 'agustus', 'september', 'oktober', 'november', 'desember'];
+bulan.splice(3, 0, 'april');
+console.log(bulan);
+
+bulan.splice(4, 1, 'mei');
+console.log(bulan);
+
+bulan.splice(4, 1);
+console.log(bulan);
+
+bulan.splice(1);
+console.log(bulan);
+let myF
